5caaea52866e173c8ffb6bc03e2d411b106f322e
sh0wer1ee <showerlee2333@gmail.com>
2021-04-26 03:31:32 +0800

    horrible audio files (5w+)

--- js/view.js
+++ js/view.js
@@ -5,6 +5,16 @@ const story_id = params.get('id');
 
 var article = document.getElementById('story');
 var storyList = [];
+var mainstory_dir = '../audio/mainstory'
+var knightsstory_dir = '../audio/knightsstory'
+var eventstory_dir = '../audio/eventstory'
+var svg_volume_up = `
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-volume-up" viewBox="0 0 16 16">
+  <path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/>
+  <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/>
+  <path d="M10.025 8a4.486 4.486 0 0 1-1.318 3.182L8 10.475A3.489 3.489 0 0 0 9.025 8c0-.966-.392-1.841-1.025-2.475l.707-.707A4.486 4.486 0 0 1 10.025 8zM7 4a.5.5 0 0 0-.812-.39L3.825 5.5H1.5A.5.5 0 0 0 1 6v4a.5.5 0 0 0 .5.5h2.325l2.363 1.89A.5.5 0 0 0 7 12V4zM4.312 6.39 6 5.04v5.92L4.312 9.61A.5.5 0 0 0 4 9.5H2v-3h2a.5.5 0 0 0 .312-.11z"/>
+</svg>
+`;
 
 /*if (story_path) { //debug use
     loadStory(story_path);
@@ -37,7 +47,15 @@ function loadStory(story_path) {
                     /*Dialogue here*/
                     var inner = '';
                     inner += '<div id="dialogue">';
-                    inner += `<div id="speaker_icon"><img src="../icons/${content.speaker_id[0]}.png" onerror="if (this.src != '../icons/DummyIcon.png') this.src = '../icons/DummyIcon.png';"/></div>`;
+                    inner += '<div id="speaker_icon">';
+                    if (content.voice_line) {
+                        inner += `<a onclick="playVoiceLine('${content.voice_line}');">`
+                        inner += `<img src="../icons/${content.speaker_id[0]}.png" onerror="if (this.src != '../icons/DummyIcon.png') this.src = '../icons/DummyIcon.png';"/>`;
+                        inner += `${svg_volume_up}</a>`;
+                    } else {
+                        inner += `<img src="../icons/${content.speaker_id[0]}.png" onerror="if (this.src != '../icons/DummyIcon.png') this.src = '../icons/DummyIcon.png';"/>`;
+                    }
+                    inner += '</div>';
                     inner += `<div id="speaker_name">${content.speaker_name}</div>`;
                     inner += `<div id="speak_content">${content.context.replace(/\\n/g, '<br>')}</div>`;
                     inner += '</div>';
@@ -144,6 +162,24 @@ function naviHtml(story_type, story_id) {
         inner = `<div id="navigator"><div id="index"><a href="../index.html#${story_type}">返回首页</a></div></div>`;
     }
     return inner;
+}
+
+function playVoiceLine(voice_line) {
+    var path = '';
+    var sp = voice_line.split('_');
+    console.log(sp[2])
+    if (sp[2] === 'MAINSTORY') {
+        path = `${mainstory_dir}/${sp[3]}${sp[4]}/${voice_line}.mp3`;
+    } else if (sp[2] === 'KNIGHTSSTORY') {
+        path = `${knightsstory_dir}/${sp[3]}${sp[4]}${sp[5]}/${voice_line}.mp3`;
+    } else if (sp[2] === 'EVENTSTORY') {
+        path = `${eventstory_dir}/${sp[3]}${sp[4]}/${voice_line}.mp3`;
+    } else {
+        console.log('wrong audio type!');
+        return;
+    }
+    var vl = new Audio(path);
+    vl.play();
 }
 
 function getTypeByID(id) {
